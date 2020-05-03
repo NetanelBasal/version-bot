70b9a99ce65bbf8c1d75faad738c87f165cb2fdd
Dan Roujinsky <danr@siemplify.co>
2020-05-03 10:17:50 +0300

    fix: set commitLink correctly

--- src/build-message/getSectionItems.ts
+++ src/build-message/getSectionItems.ts
@@ -2,23 +2,25 @@ import { ASTNode, SectionItem } from '../types';
 
 export function getSectionItems(listNode: ASTNode): SectionItem[] {
   return listNode.children.map(listItemNode => {
-    const {children} = listItemNode.children[0];
+    const { children } = listItemNode.children[0];
     let title = '';
     let i: number;
+    let commitLink;
     for (i = 0; i < children.length; i++) {
       const node = children[i];
       if (node.type === 'Link') {
+        commitLink = node;
         break;
       }
       switch (node.type) {
-        case "Strong":
+        case 'Strong':
           title += `*${node.children[0].value}*`;
           break;
         default:
           title += node.value;
       }
     }
-    const [commitLink, closesIssue, issueLink] = children.slice(1);
+    const [closesIssue, issueLink] = children.slice(2);
     const sectionItem: SectionItem = { title, commitLink };
     const isLinkedToIssue = closesIssue && closesIssue.value && closesIssue.value.includes('closes');
     if (isLinkedToIssue) {
